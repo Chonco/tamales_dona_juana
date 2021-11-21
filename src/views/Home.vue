d2c7cbf08321856955cd2985216fe40b325ad744
Carlos Diaz <kusagary_jinfire16@outlook.com>
2021-11-21 00:59:07 -0600

    Home.vue
    Home.vue finalizado!
    -Se agregaron nuevos assets
    -Se eliminaron assets sin utilizar
    -Se elimino helloworld.vue
    -CSS finalizado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,15 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bilbo+Swash+Caps&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Eczar:wght@500&display=swap" rel="stylesheet">
     <title>Tamales Doña Juana</title>
     </head>
     <body>
@@ -13,20 +22,32 @@
         </header>
         <div class="contenido">
           <div class="contenedor_articulos">
-            <div class="card">
-              <span class="titulo_articulo">Articulo 1</span>
+            <div class="card1">
+              <span class="titulo_articulo">Sobre nuestros tamales</span>
                 <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Similique placeat dolores deleniti sunt, illum, ab corrupti
-							vitae mollitia quia eius, blanditiis praesentium ratione
-							ipsam maiores amet culpa illo. Ipsum, excepturi?
+                Conoce a cerca de nuestra tradicional receta para 
+                concinar tamales de manera tradicional.
+                ¡Justo como tu abuelita los haria!
                 </p>
             </div>
 
+            <div class="card2">
+              <span class="titulo_articulo">Sobre nuestros postres</span>
+                <p>
+                ¿Interesado en algo dulce después de comer?
+                Conoce nuestra variedad de postres tradicionales. 
+                </p>
+            </div>
+
+            <div class="card3">
+              <span class="titulo_articulo">Especialidades de la casa</span>
+                <p>
+                Explora las diversas alternativas de nuestros productos.
+                </p>
+            </div>
           </div>
 
           <aside class="contenedor_publicidad">
-            <span>Aquí va la publicidad</span>
           </aside>
 
         </div>
@@ -68,6 +89,89 @@ html,body{
 	max-width: 100%;
 }
 
+header{
+  background-image: url("../assets/header_tamal2.png");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  width: 100%;
+  height: 84vh;
+  /* CSS para la fuente*/
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.elemento_header{
+  color: rgb(255, 255, 255);
+  font-size: 85px;
+  font-weight: 500;
+  margin-bottom: 1rem;
+  font-family: 'Bilbo Swash Caps', cursive;
+  font-size: 100px;
+}
+
+.contenido{
+  margin-top: 3rem;
+  display: flex;
+  width: 75%;
+}
+
+.contenedor_articulos{
+  flex: 3;
+}
+
+.card1{
+  margin-right: 10rem;
+	margin-bottom: 2rem;
+	padding: 1.5rem;
+	border-radius: 15px;
+	border: 1px solid #CED4DA;
+  background-image: url("../assets/card1.png");
+  background-repeat: no-repeat;
+	height: 100px;
+}
+
+.card2{
+  margin-right: 10rem;
+	margin-bottom: 2rem;
+	padding: 1.5rem;
+	border-radius: 15px;
+	border: 1px solid #CED4DA;
+  background-image: url("../assets/card2.png");
+  background-repeat: no-repeat;
+	height: 100px;
+}
+
+.card3{
+  margin-right: 10rem;
+	margin-bottom: 2rem;
+	padding: 1.5rem;
+	border-radius: 15px;
+	border: 1px solid #CED4DA;
+  background-image: url("../assets/card3.png");
+  background-repeat: no-repeat;
+	height: 100px;
+}
+
+.titulo_articulo{
+  border-bottom: 2px solid #ff7b00;
+  font-family: 'Kaushan Script', cursive;
+  font-size: 25px;
+  color: #522405;
+}
+
+.contenedor_publicidad{
+  flex: 1;
+	margin-left: 1rem;
+  border-radius: 15px;
+  background-image: url("../assets/publicidad_ejemplo.png");
+}
+
+p{
+  color: #000000;
+  font-family: 'Eczar', serif;
+}
+
 footer{
   align-self: end;
   display: flex;
@@ -76,5 +180,8 @@ footer{
   font-weight: bold;
   width: 85%;
 	padding: 15px;
+  font-family: 'Bilbo Swash Caps', cursive;
+  font-size: 25px;
 }
+
 </style>
