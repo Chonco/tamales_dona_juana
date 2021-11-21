eea288324478486b859c402e57391b4f41870227
Carlos Diaz <kusagary_jinfire16@outlook.com>
2021-11-20 23:30:12 -0600

    HTML y CSS
    HTML y CSS básico agregado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tamales Doña Juana</title>
+    </head>
+    <body>
+      <div class="container">
+        <header>
+          <h1 class="elemento_header">Tamales Doña Juana</h1>
+        </header>
+        <div class="contenido">
+          <div class="contenedor_articulos">
+            <div class="card">
+              <span class="titulo_articulo">Articulo 1</span>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Similique placeat dolores deleniti sunt, illum, ab corrupti
+							vitae mollitia quia eius, blanditiis praesentium ratione
+							ipsam maiores amet culpa illo. Ipsum, excepturi?
+                </p>
+            </div>
+
+          </div>
+
+          <aside class="contenedor_publicidad">
+            <span>Aquí va la publicidad</span>
+          </aside>
+
+        </div>
+
+        <footer>
+          <span>Tamales Doña Juana © 2021</span>
+        </footer>
+      </div>
+    </body>
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  
   }
 }
 </script>
+
+<style>
+html,body{
+  margin: 0;
+	padding: 0;
+	font-family: "Noto Sans", sans-serif;
+	height: 100%;
+	color: #212529;
+}
+
+.container{
+  display: grid;
+  justify-items: center;
+	height: 100%;
+	max-width: 100%;
+}
+
+footer{
+  align-self: end;
+  display: flex;
+	justify-content: center;
+  color: #f0a70a;
+  font-weight: bold;
+  width: 85%;
+	padding: 15px;
+}
+</style>
